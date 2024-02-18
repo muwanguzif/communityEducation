e7941e1aa89d71b4447755cb3e3f399cff372bce
muwanguzif <muwanguzifreedomm@gmail.com>
2024-02-18 20:54:11 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+l
+
+
+
+
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+    
+}
+
+
+
+
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
