0e3c7aa5c9a27ddb236db770ad32bce57618e164
muwanguzif <muwanguzifreedomm@gmail.com>
2024-04-01 14:54:44 +0300

    Remove 'community.Ed' folder from Git tracking

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavigationBar from './NavigationBar';
+import Home from './Home';
+import About from './About';
+import News from './News';
+import Contact from './Contact';
 
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <NavigationBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/services" component={News} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
 
-
-
-
-
-
-l
-
-
-
-
-
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'flex'
-    
-}
-
-
-
-
-function hideSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'none'
-}
+export default App;
